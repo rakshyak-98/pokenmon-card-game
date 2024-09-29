53b97da4e4e4480e2216234f4522b85158bb7a5d
rakshyak-98 <rakshak1998@gmail.com>
2024-09-29 21:00:10 +0530

    add login component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import AuthForms from "./components/custom/Form/AuthForm";
 import Navbar from "./components/custom/Navbar";
 import PokemonCollection from "./components/custom/PokemonCollection";
 
 function App() {
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	useEffect(() => {
+		const token = localStorage.getItem("token");
+		if (token) {
+			setIsAuthenticated(true);
+		}
+	}, []);
 	return (
 		<>
-			<Navbar />
-			<PokemonCollection />
+			{isAuthenticated ? (
+				<>
+					<Navbar />
+					<PokemonCollection />
+				</>
+			) : (
+				<AuthForms />
+			)}
 		</>
 	);
 }
